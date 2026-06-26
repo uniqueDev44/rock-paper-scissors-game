94319634128198fa06564d61b7011c13ccb51f0a
uniqueDev44 <uniquedev44@gmail.com>
2026-06-25 21:14:19 -0700

    play game with keyboard

--- scripts/rock-paper-scissors.js
+++ scripts/rock-paper-scissors.js
@@ -109,6 +109,15 @@
       updateScore();
     }
 
+    document.body.addEventListener('keydown', (event) => {
+      if (event.key === 'r') {
+        playGame('rock')
+      }else if (event.key === 'p') {
+        playGame('paper')
+      }else if (event.key === 's') {
+        playGame('scissors')
+      }
+    })
 
     function updateScore() {
       document.querySelector('.js-score')
